21f550a568633b248de890d33673c5f27a1e2bd3
Stephen <stephenh.email@gmail.com>
2018-09-02 16:21:04 -0400

    started implimenting opentok

---
+++ Project2Server/src/main/java/com/opentok/constants/Version.java
@@ -0,0 +1,12 @@
+/**
+ * OpenTok Java SDK
+ * Copyright (C) 2018 TokBox, Inc.
+ * http://www.tokbox.com
+ *
+ * Licensed under The MIT License (MIT). See LICENSE file for more information.
+ */
+package com.opentok.constants;
+
+public class Version {
+    public static final String VERSION = "4.2.0";
+}
